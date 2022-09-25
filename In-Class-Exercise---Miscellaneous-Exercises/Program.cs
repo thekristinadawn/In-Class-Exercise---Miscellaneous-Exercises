54dafbd92274a0dcab77defffd121be982d60fbb
Kristina Palmer <thekristinadawn@gmail.com>
2022-09-25 16:39:51 -0500

    Started Labs

---
+++ In-Class-Exercise---Miscellaneous-Exercises/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace In_Class_Exercise___Miscellaneous_Exercises_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int sizeArray;
+            Console.WriteLine("Enter Size of Array:");
+            sizeArray = Convert.ToInt32(Console.ReadLine());
+            int[] Array1 = new int[sizeArray];
+
+            for (int i = 0; i < sizeArray; i++)
+            {
+                Array1[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            //for (int i = 0;i < sizeArray; i++)
+            //{
+                //Console.WriteLine(Array1[i]);
+            //}
+
+        }
+    }
+}
